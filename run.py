a8c241cc6092f57b13de026202aa0849d1187bff
Anne Voretzsch <ac.voretzsch@gmx.de>
2024-10-03 15:43:01 +0000

    Calculate surplus data

--- run.py
+++ run.py
@@ -103,8 +103,15 @@ def calculate_surplus_data(sales_row):
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
-
+    
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        # A list comprehension could be used to parse all strings in the list to integers, 
+        # however this method converts the value at the same time it is accessed
+        surplus_data.append(surplus)
+    
+    return surplus_data
 
 def main():
     """
@@ -113,7 +120,8 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
     # Common practice to wrap all function calls in a main function
 
 print("Welcome to Love Sandwiches Data Automation")
